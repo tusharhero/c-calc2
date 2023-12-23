78f512dacffb301f401172e5abd38e5bef5e263c
tusharhero <tusharhero@sdf.org>
2023-12-23 15:07:58 +0530

    Add '^' exponentiation operator.

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -84,6 +85,11 @@ evaluate (char *expression, size_t length)
             case '/':
               {
                 solution /= number.value;
+                break;
+              }
+            case '^':
+              {
+                solution = pow (solution, number.value);
                 break;
               }
             }
